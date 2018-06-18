8ee829797103e83102fd8a58ad7c08dc3f7938bc
akichan22 <sugawara256@gmail.com>
2018-06-19 00:03:57 +0900

    parse string

--- c2g.go
+++ c2g.go
@@ -4,11 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	rep := regexp.MustCompile(`^(\s*)([0-9]+) (.*)$`)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := rep.FindSubmatch([]byte(scanner.Text()))
+		spacer := string(line[1])
+		count := line[2]
+		text := string(line[3])
+
+		fmt.Println(spacer)
+		fmt.Println(count)
+		fmt.Println(text)
 	}
 }
